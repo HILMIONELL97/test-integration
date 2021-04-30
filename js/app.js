924bd8a0e4dcd7750748b7981b7b1d47151207bc
HILMI ONELL <57817028+HILMIONELL97@users.noreply.github.com>
2021-04-30 22:22:38 +0000

    Done !

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 let userName = document.getElementById('myName');
 let password = document.getElementById('myPsw');
+let confirmPassword = document.getElementById('myConfirmPsw')
+let email = document.getElementById('myEmail')
 let form = document.getElementById('form');
 
 
@@ -11,13 +13,31 @@ form.addEventListener('submit', e => {
 
 function checkInputs() {
     // trim to remove the whitespaces
-
+    const emailValue = email.value.trim();
+    const validEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const passwordValue = password.value.trim();
+    const confirmPswValue = confirmPassword.value.trim();
 
-    if (passwordValue === '') {
-        setErrorFor(password, 'le mot de passe doit comporter entre 4 et 6 caractères');
+
+    if (emailValue !== validEmail) {
+        setErrorFor(email, 'Veuillez saisir une adresse e-mail valide')
+        return
+    } else {
+        setSuccessFor(email)
+    }
+    if (passwordValue.length <= 6) {
+
+        setErrorFor(password, 'Le mot de passe doit comporter entre 4 et 6 caractères');
+        return
     } else {
         setSuccessFor(password);
+    }
+    if (confirmPswValue !== passwordValue) {
+        setErrorFor(confirmPassword, 'Le mot de passe ne correspond pas');
+        return
+
+    } else {
+        alert('Vous vous êtes inscrit avec succès')
     }
 
 
@@ -33,4 +53,13 @@ function setErrorFor(input, message) {
 function setSuccessFor(input) {
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
+}
+
+
+
+function eye(id) {
+    if (document.getElementById(id).type == "text") {
+        return (document.getElementById(id).type = "password");
+    }
+    return (document.getElementById(id).type = "text");
 }
